0123f4354f9d8964d0e211c6bd1e71784f4ea0cf
mec140130 <mec140130@utdallas.edu>
2019-06-28 00:51:13 -0500

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src = {logo} lat= "logo" />
-      <h2>Let's develop a management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id': 1,
+    'image': "http://placeimg.com/64/64/8",
+    'name': 'Albert',
+    'birthday': '961222',
+    'gender': 'male',
+    'job': 'student'
+  },
+  {
+    'id': 2,
+    'image': "http://placeimg.com/64/64/2",
+    'name': 'Lisa',
+    'birthday': '871106',
+    'gender': 'female',
+    'job': 'programmer'
+  },
+  {
+    'id': 3,
+    'image': "http://placeimg.com/64/64/3",
+    'name': 'Mary',
+    'birthday': '960427',
+    'gender': 'female',
+    'job': 'accountant'
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key = {c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
